86df2e660b7285743c20c3470e07a942bc9ae341
jrodass <jrodass@users.noreply.github.com>
2018-03-31 16:02:18 +0200

    Commit #5
    Actualizacion de las rutas.

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -2,7 +2,12 @@ from random import randint
 
 '''Rutas de los archivos que lee el proyecto de RESDEC'''
 FILE_PATH_ANDROID = "../data/android.data"
-FILE_PATH_WORDPRESS = "../data/wordpress_ini.data"
-FILE_PATH_WORDPRESS_FORMATTED = "../data/wordpress_" + str(randint(0, 1000)) + ".data"
+FILE_PATH_WORDPRESS = "../data/ratings_0221.csv"
+FILE_PATH_WORDPRESS_CONTENT = "../data/plugin_tag_0221.csv"
 FILE_PATH_ANDROID_KNN = "../data/product_features.item"
 FILE_PATH_EXAMPLE_CONTENT = "../data/pubmed_oa_2013_2.csv"
+FILE_PATH_WORDPRESS_CORREGIDA = "../generated/ratings_corregido.csv"
+
+
+def file_path_formatted(file_name):
+    return "../generated/" + file_name + "_" + str(randint(0, 1000)) + ".csv"
